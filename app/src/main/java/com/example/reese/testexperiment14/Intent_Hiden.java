5a8201e18fb0a61720c99333d7b60eca14d27418
reeseyuan <yuanruisi0119@gmail.com>
2018-05-03 10:56:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/reese/testexperiment14/Intent_Hiden.java
@@ -0,0 +1,14 @@
+package com.example.reese.testexperiment14;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Intent_Hiden extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_intent);
+
+    }
+}
